c9dcfbbeba9cb0b1e17f907a2982380ac9eb62f1
montanon <montagna.p.92@hotmail.com>
2025-03-18 23:49:18 -0300

    feat: add plotting imports

---
+++ mitoolspro/plotting/__init__.py
@@ -0,0 +1,19 @@
+from mitoolspro.plotting.composers import AxesComposer, PlotComposer
+from mitoolspro.plotting.plots.bar_plotter import BarPlotter
+from mitoolspro.plotting.plots.box_plotter import BoxPlotter
+from mitoolspro.plotting.plots.distribution_plotter import DistributionPlotter
+from mitoolspro.plotting.plots.error_plotter import ErrorPlotter
+from mitoolspro.plotting.plots.histogram_plotter import HistogramPlotter
+from mitoolspro.plotting.plots.line_plotter import LinePlotter
+from mitoolspro.plotting.plots.matplotlib_typing import (
+    COLORS,
+    Cmap,
+    Color,
+    EdgeColor,
+    MarkerStyle,
+    Norm,
+    Scale,
+)
+from mitoolspro.plotting.plots.pie_plotter import PiePlotter
+from mitoolspro.plotting.plots.plotter import Plotter
+from mitoolspro.plotting.plots.scatter_plotter import ScatterPlotter
