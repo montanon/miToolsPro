61c11286029cde5d5e7ef64a918adcc16ca795e2
montanon <montagna.p.92@hotmail.com>
2025-03-20 16:01:16 -0300

    feat: add ModelRegistry to OpenAITokensCounter

--- mitoolspro/llms/clients.py
+++ mitoolspro/llms/clients.py
@@ -9,6 +9,7 @@ from openai.types.chat import ChatCompletion
 from mitoolspro.exceptions import ArgumentValueError
 from mitoolspro.llms.objects import (
     LLMModel,
+    ModelRegistry,
     PersistentTokensCounter,
     Prompt,
     TokenUsageStats,
@@ -65,25 +66,18 @@ class OpenAIClient(LLMModel):
 
 
 class OpenAITokensCounter(PersistentTokensCounter):
-    COST_PER_1M_TOKENS = {
-        "gpt-3.5-turbo": {"input": 3.0, "output": 8.0},
-        "gpt-4o-mini": {"input": 0.15, "output": 0.6},
-        "gpt-4o": {"input": 2.5, "output": 10.0},
-        "o1-preview": {"input": 15.0, "output": 60.0},
-        "o1": {"input": 15.0, "output": 60.0},
-        "o1-mini": {"input": 3.0, "output": 12.0},
-    }
-
     def __init__(self, file_path: Path, model: str = "gpt-4o-mini"):
         if model not in self.COST_PER_1M_TOKENS.keys():
             raise ArgumentValueError(
                 f"Model {model} not supported, must be one of {self.COST_PER_1M_TOKENS.keys()}"
             )
         self.model = model
+        registry = ModelRegistry.get_instance("openai")
+        self.model_costs = registry.get_model_costs(self.model)
         super().__init__(
             file_path=file_path,
-            cost_per_1M_input_tokens=self.COST_PER_1M_TOKENS[model]["input"],
-            cost_per_1M_output_tokens=self.COST_PER_1M_TOKENS[model]["output"],
+            cost_per_1M_input_tokens=self.model_costs["input"],
+            cost_per_1M_output_tokens=self.model_costs["output"],
         )
 
     def get_usage_stats(self, response: ChatCompletion) -> TokenUsageStats:
