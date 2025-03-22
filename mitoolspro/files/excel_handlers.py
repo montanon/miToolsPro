f59ed3b15d21a870e70cfcdeeb0349213d714590
montanon <montagna.p.92@hotmail.com>
2025-03-22 18:05:08 -0300

    feat: add few excel_handlers

---
+++ mitoolspro/files/excel_handlers.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+import openpyxl
+from openpyxl.worksheet.worksheet import Worksheet
+
+
+def auto_adjust_excel_columns_width(excel_path: Path) -> None:
+    book = openpyxl.load_workbook(excel_path)
+    for sheet_name in book.sheetnames:
+        sheet = book[sheet_name]
+        auto_adjust_sheet_columns_width(sheet)
+    book.save(excel_path)
+
+
+def auto_adjust_sheet_columns_width(sheet: Worksheet) -> None:
+    for column in sheet.columns:
+        max_length = 0
+        column = [cell for cell in column if cell.value]  # Filter out None values
+        for cell in column:
+            try:
+                if len(str(cell.value)) > max_length:
+                    max_length = len(str(cell.value))
+            except Exception:
+                pass
+        adjusted_width = max_length + 1  # Adding a little extra width
+        sheet.column_dimensions[
+            openpyxl.utils.get_column_letter(column[0].column)
+        ].width = adjusted_width
