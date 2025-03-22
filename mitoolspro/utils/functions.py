da42eb1405630a828b1a9cd53f65b535ffd2548f
montanon <montagna.p.92@hotmail.com>
2025-03-22 11:56:46 -0300

    feat: add decorators

--- mitoolspro/utils/functions.py
+++ mitoolspro/utils/functions.py
@@ -1,7 +1,19 @@
 import re
+from typing import Generator, Iterable
 
 
 def remove_characters_from_string(string: str, characters: str = None) -> str:
     if characters is None:
         characters = r'[\\/*?%&:"<>|]'
     return re.sub(characters, "", string)
+
+
+def iterable_chunks(
+    iterable: Iterable, chunk_size: int
+) -> Generator[Iterable, None, None]:
+    if not isinstance(iterable, (str, list, tuple, bytes)):
+        raise TypeError(
+            f"Provided iterable of type {type(iterable).__name__} doesn't support slicing."
+        )
+    for i in range(0, len(iterable), chunk_size):
+        yield iterable[i : i + chunk_size]
