1e21014b2bb80f6c9610665d3118c6f36e027e28
montanon <montagna.p.92@hotmail.com>
2025-04-12 19:47:18 -0400

    feat: remove unused definitions

--- mitoolspro/nlp/spacy_utils.py
+++ mitoolspro/nlp/spacy_utils.py
@@ -1,6 +1,4 @@
 import unicodedata
-from dataclasses import dataclass, field
-from typing import List, Set, Tuple
 
 
 def _strip_accents(text: str) -> str:
@@ -9,39 +7,3 @@ def _strip_accents(text: str) -> str:
         for ch in unicodedata.normalize("NFD", text)
         if unicodedata.category(ch) != "Mn"
     )
-
-
-@dataclass
-class CategoryMatches:
-    name: str
-    matches: List[str]
-    strip_accents: bool = True
-    lowercase: bool = True
-    single_matches: Set[str] = field(init=False)
-    multi_matches: List[Tuple[str, ...]] = field(init=False)
-
-    def __post_init__(self):
-        self.single_matches = set()
-        self.multi_matches = []
-
-        for text in self.matches:
-            if self.strip_accents:
-                text = _strip_accents(text)
-            if self.lowercase:
-                text = text.lower()
-
-            matches = tuple(text.split())
-            if len(matches) == 1:
-                self.single_matches.add(matches[0])
-            else:
-                self.multi_matches.append(matches)
-
-
-def normalize_text(
-    text: str, strip_accents: bool = True, lowercase: bool = True
-) -> str:
-    if strip_accents:
-        text = _strip_accents(text)
-    if lowercase:
-        text = text.lower()
-    return text
