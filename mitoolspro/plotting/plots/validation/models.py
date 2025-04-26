05f020604e99cd0d0627c7fec11ddc9c20bd1bec
montanon <montagna.p.92@hotmail.com>
2025-04-26 13:45:06 -0400

    feat: add SequenceParam and SequencesParam

--- mitoolspro/plotting/plots/validation/models.py
+++ mitoolspro/plotting/plots/validation/models.py
@@ -4,7 +4,7 @@ from typing import Annotated, Any, Generic, Literal, TypeAlias, TypeVar, Union
 import numpy as np
 from matplotlib.colors import Colormap, Normalize
 from matplotlib.markers import MarkerStyle
-from numpy import integer
+from numpy import integer, ndarray
 from pandas import Series
 from pydantic import BaseModel, ConfigDict, ValidationError, model_validator
 from typing_extensions import TypeGuard
@@ -22,10 +22,6 @@ from mitoolspro.plotting.plots.matplotlib_typing import (
     MARKERS_FILLSTYLES,
     NORMALIZATIONS,
     NumericSequences,
-)
-from mitoolspro.plotting.plots.validation.schemas import (
-    NumpyArray,
-    PandasSeries,
 )
 
 T = TypeVar("T")
@@ -57,38 +53,90 @@ class DictParam(Param[dict]):
 class SequenceParam[T](Param[Sequence[T]]):
     value: Sequence[T]
 
+    @model_validator(mode="before")
+    @classmethod
+    def validate_type(cls, values: Any) -> dict:
+        if isinstance(values, dict):
+            values = values["value"]
+        if isinstance(values, (ndarray, Series)):
+            values = values.tolist()
+        elif isinstance(values, tuple):
+            values = list(values)
+        if not isinstance(values, Sequence):
+            raise ArgumentValidationError(f"Expected Sequence, got {type(values)}")
+        return {"value": values}
+
+
+class SequencesParam[T](Param[SequenceParam[SequenceParam[T]]]):
+    value: SequenceParam[SequenceParam[T]]
+
+    @model_validator(mode="before")
+    @classmethod
+    def standardize_input(cls, values: Any) -> dict:
+        if isinstance(values, dict):
+            input_value = values["value"]
+        else:
+            input_value = values
+
+        if isinstance(input_value, (ndarray, Series)):
+            input_value = input_value.tolist()
+        if not isinstance(input_value, Sequence):
+            raise ArgumentValidationError(
+                f"Expected a Sequence, got {type(input_value)}"
+            )
+
+        normalized = []
+        for value in input_value:
+            if isinstance(value, (list, tuple, ndarray, Series)):
+                if isinstance(value, (ndarray, Series)):
+                    value = value.tolist()
+                elif isinstance(value, tuple):
+                    value = list(value)
+                sequence_param = SequenceParam[T](value=value)
+            elif isinstance(value, SequenceParam):
+                sequence_param = value
+            else:
+                raise ArgumentValidationError(
+                    f"Expected a SequenceParam[T] or raw sequence, got {type(value)}"
+                )
+            normalized.append(sequence_param)
+
+        return {"value": normalized}
+
+    # @model_validator(mode="after")
+    # def validate_element_types(self) -> "SequencesParam[T]":
+    #     expected_type = self.__pydantic_generic_metadata__["args"][0]
+
+    #     for sequence_param in self.value:
+    #         for item in sequence_param.value:
+    #             if not isinstance(item, expected_type):
+    #                 raise ArgumentValidationError(
+    #                     f"Expected inner element of type {expected_type}, got {type(item)}"
+    #                 )
+    #     return self
+
 
 if __name__ == "__main__":
-    print(NumericParam(value=0))
-    print(NumericParam(value=0.0))
-    print(NumericParam(value=-1))
-    print(NumericParam(value=-1.0))
-    print(NumericParam(value=1))
-    print(NumericParam(value=1.0))
-    try:
-        print(NumericParam(value="abcd"))
-    except ValidationError as e:
-        print(e)
-    print(StrParam(value="abcd"))
-    try:
-        print(StrParam(value=1))
-    except ValidationError as e:
-        print(e)
-    print(BoolParam(value=True))
-    try:
-        print(BoolParam(value="abcd"))
-    except ValidationError as e:
-        print(e)
-    print(DictParam(value={"a": 1, "b": 2}))
-    try:
-        print(DictParam(value=1))
-    except ValidationError as e:
-        print(e)
     print(SequenceParam(value=[1, 2, 3, "abcd", True, False]))
     print(SequenceParam(value=(1, 2, 3, "abcd", True, False)))
     print(SequenceParam(value=np.array([1, 2, 3, "abcd", True, False])))
     print(SequenceParam(value=Series([1, 2, 3, "abcd", True, False])))
     try:
         print(SequenceParam(value=1))
+    except ValidationError as e:
+        print(e)
+    print(SequencesParam(value=[[1, 2, 3], [4, 5, 6]]))
+    print(
+        SequencesParam(
+            value=[
+                [1, 2, 3],
+                np.array([4, 5, 6]),
+                Series([7, 8, 9]),
+                tuple([10, "abcd", 12]),
+            ]
+        )
+    )
+    try:
+        print(SequencesParam(value=1))
     except ValidationError as e:
         print(e)
