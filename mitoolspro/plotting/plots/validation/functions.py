a666bd550d4c1c33c0e83e69e3fed3338d270bce
montanon <montagna.p.92@hotmail.com>
2025-04-26 22:19:40 -0400

    feat: add is_marker

--- mitoolspro/plotting/plots/validation/functions.py
+++ mitoolspro/plotting/plots/validation/functions.py
@@ -1,13 +1,16 @@
+from pathlib import Path
 from typing import Any
 
-from matplotlib.colors import (
-    get_named_colors_mapping,
-    is_color_like,
-)
+from matplotlib.colors import Normalize, get_named_colors_mapping, is_color_like
+from matplotlib.markers import MarkerStyle
 from numpy import ndarray
 from pandas import Series
+from typing_extensions import TypeAlias
 
+NumericType: TypeAlias = float | int
 COLORS = set(get_named_colors_mapping().keys())
+MARKERS = set(MarkerStyle.markers.keys()).union(set(MarkerStyle.filled_markers))
+MARKERS_FILLSTYLES = set(MarkerStyle.fillstyles)
 
 
 def is_indexable(value: Any, index: Any) -> bool:
@@ -16,6 +19,10 @@ def is_indexable(value: Any, index: Any) -> bool:
         return True
     except (TypeError, IndexError, KeyError):
         return False
+
+
+def is_value_in_range(value: Any, min_value: NumericType, max_value: NumericType):
+    return isinstance(value, NumericType) and min_value <= value and value <= max_value
 
 
 def coerce_to_list(value: Any) -> Any:
@@ -73,3 +80,49 @@ def is_color(value: Any) -> bool:
     return (
         is_color_like(value) or is_color_none(value) or is_color_numeric_scalar(value)
     )
+
+
+def is_marker(value: Any) -> bool:
+    if isinstance(value, (str, int, Path, MarkerStyle, dict)):
+        if isinstance(value, str):
+            return value in MARKERS
+        if isinstance(value, int):
+            return is_value_in_range(value, 0, 11)
+        if isinstance(value, dict):
+            valid_keys = all(
+                key in ["marker", "fillstyle", "transform", "capstyle", "joinstyle"]
+                for key in value
+            )
+            valid_marker = value["marker"] in MARKERS if "marker" in value else True
+            valid_fillstyle = (
+                value["fillstyle"] in MARKERS_FILLSTYLES
+                if "fillstyle" in value
+                else True
+            )
+            valid_transform = (
+                isinstance(value["transform"], (str, Normalize))
+                if "transform" in value
+                else True
+            )
+            valid_capstyle = (
+                value["capstyle"] in ["butt", "round", "projecting"]
+                if "capstyle" in value
+                else True
+            )
+            valid_joinstyle = (
+                value["joinstyle"] in ["miter", "round", "bevel"]
+                if "joinstyle" in value
+                else True
+            )
+            return (
+                valid_keys
+                and valid_marker
+                and valid_fillstyle
+                and valid_transform
+                and valid_capstyle
+                and valid_joinstyle
+            )
+        return isinstance(value, (Path, MarkerStyle))
+    elif value is None:
+        return True
+    return False
