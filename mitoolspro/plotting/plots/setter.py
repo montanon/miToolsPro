a491b11d595700fc896614e46568be4d7fa6b1ef
montanon <montagna.p.92@hotmail.com>
2025-05-12 16:32:23 -0400

    feat: add n_sequences and single_param functionality to setter

--- mitoolspro/plotting/plots/setter.py
+++ mitoolspro/plotting/plots/setter.py
@@ -90,6 +90,11 @@ class SetterMixIn(ABC):
     def multi_data(self) -> bool:
         pass
 
+    @property
+    @abstractmethod
+    def n_sequences(self) -> int:
+        pass
+
     def _calculate_sizes(
         self, x_data: Sequence[Sequence[Any]], multi_data: bool
     ) -> tuple[SizesType, SizesType]:
@@ -159,6 +164,7 @@ class SetterMixIn(ABC):
         sequences: Union[NumericSequences, NumericSequence, NumericType],
         param_name: str,
         multi_param: bool = True,
+        single_param: bool = True,
         min_value: NumericType = None,
         max_value: NumericType = None,
         structured: bool = True,
@@ -196,13 +202,13 @@ class SetterMixIn(ABC):
             sequences = (
                 NumericSequenceParam(
                     value=sequences,
-                    sizes=self.sizes,
+                    sizes=self.sizes if single_param else self.n_sequences,
                     structured=structured,
                 )
                 if no_range
                 else RangeSequenceParam(
                     value=sequences,
-                    sizes=self.sizes,
+                    sizes=self.sizes if single_param else self.n_sequences,
                     min_value=min_value,
                     max_value=max_value,
                     structured=structured,
@@ -212,18 +218,19 @@ class SetterMixIn(ABC):
             return self
         except ValidationError as e:
             last_error = str(e)
-        try:
-            sequences = (
-                NumericParam(value=sequences)
-                if no_range
-                else RangeParam(
-                    value=sequences, min_value=min_value, max_value=max_value
-                )
-            ).value
-            setattr(self, param_name, sequences)
-            return self
-        except ValidationError as e:
-            last_error = str(e)
+        if single_param:
+            try:
+                sequences = (
+                    NumericParam(value=sequences)
+                    if no_range
+                    else RangeParam(
+                        value=sequences, min_value=min_value, max_value=max_value
+                    )
+                ).value
+                setattr(self, param_name, sequences)
+                return self
+            except ValidationError as e:
+                last_error = str(e)
 
         if self.multi_data:
             msg = (
@@ -391,6 +398,7 @@ class SetterMixIn(ABC):
         sequences: Union[StrSequences, StrSequence, str],
         param_name: str,
         multi_param: bool = True,
+        single_param: bool = True,
         structured: bool = True,
     ):
         if self.multi_data and multi_param:
@@ -408,19 +416,20 @@ class SetterMixIn(ABC):
         try:
             sequences = StrSequenceParam(
                 value=sequences,
-                sizes=self.sizes,
+                sizes=self.sizes if single_param else self.n_sequences,
                 structured=structured,
             ).value
             setattr(self, param_name, sequences)
             return self
         except ValidationError as e:
             last_error = str(e)
-        try:
-            sequences = StrParam(value=sequences).value
-            setattr(self, param_name, sequences)
-            return self
-        except ValidationError as e:
-            last_error = str(e)
+        if single_param:
+            try:
+                sequences = StrParam(value=sequences).value
+                setattr(self, param_name, sequences)
+                return self
+            except ValidationError as e:
+                last_error = str(e)
         if self.multi_data:
             msg = (
                 f"Invalid {param_name}. Expected a string, a sequence of strings, "
