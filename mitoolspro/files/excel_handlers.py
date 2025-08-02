5ecb8315fb76659179d8014670c81e49b652bdbe
montanon <montagna.p.92@hotmail.com>
2025-08-02 11:10:00 -0400

    chore: add max_len to adjust excel cols

--- mitoolspro/files/excel_handlers.py
+++ mitoolspro/files/excel_handlers.py
@@ -4,15 +4,19 @@ import openpyxl
 from openpyxl.worksheet.worksheet import Worksheet
 
 
-def auto_adjust_excel_columns_width(excel_path: Path) -> None:
+def auto_adjust_excel_columns_width(
+    excel_path: Path, max_len: int | None = None
+) -> None:
     book = openpyxl.load_workbook(excel_path)
     for sheet_name in book.sheetnames:
         sheet = book[sheet_name]
-        auto_adjust_sheet_columns_width(sheet)
+        auto_adjust_sheet_columns_width(sheet, max_len)
     book.save(excel_path)
 
 
-def auto_adjust_sheet_columns_width(sheet: Worksheet) -> None:
+def auto_adjust_sheet_columns_width(
+    sheet: Worksheet, max_len: int | None = None
+) -> None:
     for column in sheet.columns:
         max_length = 0
         column = [cell for cell in column if cell.value]  # Filter out None values
@@ -21,7 +25,7 @@ def auto_adjust_sheet_columns_width(sheet: Worksheet) -> None:
         for cell in column:
             try:
                 cell_len = max([len(line) for line in str(cell.value).split("\n")])
-                if cell_len > max_length:
+                if cell_len > max_length and (max_len is None or cell_len < max_len):
                     max_length = cell_len
             except Exception:
                 pass
